a5f36732079334e123b42a56d5c8d21267fd3c19
Blake Kostner <git@btkostner.io>
2021-08-11 13:44:14 -0600

    feat: init action to find flatpak manifest

---
+++ src/index.js
@@ -0,0 +1,57 @@
+const core = require('@actions/core')
+const { stat } = require('fs')
+const { relative, resolve } = require('path')
+
+function fileExists(path) {
+  return new Promise((resolve, reject) => {
+    stat(path, (err, stats) => {
+      if (err) {
+        return reject(err)
+      } else {
+        return resolve(stat.isFile())
+      }
+    })
+  })
+}
+
+async function findManifest(workspace, rdnn) {
+  const possibleFiles = [
+    `${rdnn}.json`,
+    `${rdnn}.yml`,
+    `${rdnn}.yaml`
+  ]
+
+  for (const p in possibleFiles) {
+    const fullPath = resolve(workspace, p)
+
+    if (await fileExists(fullPath)) {
+      return relative(workspace, fullPath)
+    }
+  }
+
+  return null
+}
+
+async function run () {
+  const rdnn = core.getInput('rdnn', { required: true })
+  const workspace = core.getInput('workspace', { required: true })
+
+  const manifest = await findManifest(workspace, rdnn)
+
+  if (manifest == null) {
+    core.setFailed('Unable to find manifest file')
+  } else {
+    core.info(`Found manifest: ${manifest}`)
+
+    core.setOutput('manifest', manifest)
+  }
+}
+
+;(async () => {
+  try {
+    await run()
+  } catch (error) {
+    core.error(error)
+    core.setFailed(error.message)
+  }
+})()
